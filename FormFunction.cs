f1f9a7f16d4356b2cc7b394ff448c46e39b64c9a
wavalava <86676079+wavalava@users.noreply.github.com>
2022-03-16 13:52:48 -0400

    Create FormFunction.cs

---
+++ FormFunction.cs
@@ -0,0 +1,48 @@
++﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NatsukiV2
+{
+    public partial class PopGUI : Form
+    {
+        public PopGUI()
+        {
+            InitializeComponent();
+            MessageBox.Show("Note: This version of Natsuki does not have any functioning cheats/hacks.");
+            
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {  
+
+        }
+
+        private void PopGUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
